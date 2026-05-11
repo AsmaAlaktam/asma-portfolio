8cffa25d037d8145855da2f06d498a7123adffd7
AsmaAlaktam <asmaeee2003@gmail.com>
2026-05-11 18:52:01 +0300

    Add files via upload

---
+++ asma.js
@@ -0,0 +1,56 @@
+const contactForm = document.getElementById('content');
+
+contactForm.addEventListener('submit', function(event) {
+    
+    event.preventDefault();
+
+    alert('Thank you for contacting me! I will get back to you soon.');
+
+
+    contactForm.reset();
+});
+
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-theme');
+
+    const icon = darkModeToggle.querySelector('i');
+    if (body.classList.contains('dark-theme')) {
+        icon.classList.replace('fa-moon', 'fa-sun');
+    } else {
+        icon.classList.replace('fa-sun', 'fa-moon');
+    }
+});
+
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop - 80, 
+            behavior: 'smooth'
+        });
+        
+    });
+});
+
+const backToTopBtn = document.getElementById('backToTop');
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+};
+
+backToTopBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
